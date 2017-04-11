ca11cd436efae3702f654ad4f00c6005d5d4f118
SL-prog <supp0rt.goog1e@gmail.com>
2017-04-12 01:37:49 +0200

    first test
    some bugs

---
+++ flip.py
@@ -0,0 +1,85 @@
+import pygame
+from pygame.locals import *
+pygame.init()
+
+window = pygame.display.set_mode((500, 500))
+pygame.display.set_caption("FLIP")
+
+class Case:
+    def __init__(self, IMAGE, posx, posy):#, numero):
+        self.img = pygame.image.load(IMAGE)
+        self.posx = posx
+        self.posy = posy
+        self.clicked = False
+        self.moovex = 0
+        self.moovey = 0
+        #self.numero = numero
+
+    def clic(self, mousex, mousey):
+        videx = case[0].posx
+        videy = case[0].posy
+
+        if (self.posx <= mousex <= self.posx + 125):
+            if (self.posy <= mousey <= self.posy + 125):
+                self.clicked = True
+        else:
+            self.clicked = False
+
+        if self.clicked:
+            if ((self.posx - 125) == videx) and (self.posy == videy):
+                    self.moovex = -125
+            elif ((self.posy - 125) == videy) and (self.posx == videx):
+                    self.moovey = -125
+            elif ((self.posx + 125) == videx) and (self.posy == videy):
+                    self.moovex = 125
+            elif ((self.posy + 125) == videy) and (self.posx == videx):
+                    self.moovey = 125
+
+        if self.moovex != 0:
+            self.MooveX()
+
+        if self.moovey != 0:
+            self.MooveY()
+
+    def MooveX(self):
+        self.posx = case[0].posx
+        case[0].posx-=self.moovex
+        self.moovex = 0
+
+    def MooveY(self):
+        self.posy = case[0].posy
+        case[0].posy-=self.moovey
+        self.moovey = 0
+
+    def blit(self, window):
+        window.blit(self.img, (self.posx, self.posy))
+
+case = [0]*16
+i = 0
+for y in range(0, 4):
+    for x in range(0, 4):
+        case[i] = Case(str(i)+".png", x*125, y*125) #, i)
+        i+=1
+
+loop = True
+while loop:
+    window.fill((255,255,255))
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            loop = False
+        if event.type == MOUSEBUTTONDOWN and event.button == 1 :
+            i = 0
+            for y in range(0, 4):
+                for x in range(0, 4):
+                    case[i].clic(event.pos[0], event.pos[1])
+                    i+=1
+    i = 0
+    for y in range(0, 4):
+        for x in range(0, 4):
+            case[i].blit(window)
+            i+=1
+
+
+    pygame.display.flip()
+pygame.quit()
